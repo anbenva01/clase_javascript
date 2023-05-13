443f3d95e2166f8353d6a3598edb4f0986b89d80
ANDRES <andresb@example.com>
2023-05-13 10:19:54 -0500

    variables, comentarios y constantes

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-// hola mundo
-alert('hola mundo');
 
-/* comentarios*/
 
-alert('hola'); alert('mundo');
+//variables
+let a = 5;
+let b =6;
 
+let suma = a+b;
+console.log('el resultado de la suma es'+suma);
+
+let nombre = 'roberto';
+let nombre_dos = nombre;
+
+nombre_dos= 'carlos';
+
+console.log('primer nombre',nombre);
+console.log('segundo nombre',nombre_dos);
+
+//notaciones de variables
+
+camelCase = 5;
+snake_case = 5;
+Pascal_case = 5;
+
+let nombre_completo = 'Andres camilo benjumea';
+
+// no puedes iniciar con numero, no puedes tener guion medio
+
+//constantes
+const PI = 3.1416;
+const COLOR = 'rojo';
